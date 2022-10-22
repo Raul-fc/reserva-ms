f01ed09bcc809c294b1e3157aea8c19a618ab2e9
Raul Fulguera Colque <raul.e.fulguera@gmail.com>
2022-10-22 10:36:17 -0400

    test change

--- Reservas.Application/UseCases/Command/Reservas/CrearReserva/CrearReservaHandler.cs
+++ Reservas.Application/UseCases/Command/Reservas/CrearReserva/CrearReservaHandler.cs
@@ -35,7 +35,7 @@ namespace Reservas.Application.UseCases.Command.Reservas.CrearReserva {
 
 			if (nroAsientosDisponibles < request.Detalle.Count)
 			{
-				throw new BussinessRuleValidationException($"Solo se dispone de : {nroAsientosDisponibles} asientos");
+				throw new BussinessRuleValidationException($"Se tiene {nroAsientosDisponibles} , asientos disponible");
 			}
 			Reserva objReserva = _reservaFactory.Create(request.IdVuelo, nroReserva, request.FechaVuelo);
 
